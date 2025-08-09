34433929fe980dcfd8513795beed3acd21a2e076
saiharshini@7 <narinasaiharshini@gmail.com>
2025-08-09 12:58:36 +0530

    commit

--- hello.py
+++ hello.py
@@ -4,3 +4,11 @@ if num_1>num_2 :
  print("num_1 is biggest")
 else:
  print("num_2 is biggest")
+
+
+num_1 = 10
+num_2 = 20
+if num_1>num_2 :
+ print("num_1 is biggest")
+else:
+ print("num_2 is biggest")
